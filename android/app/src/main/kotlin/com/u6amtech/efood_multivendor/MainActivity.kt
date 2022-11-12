3b91031d48ac4afc969357998d3b418c195f350a
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2022-11-12 20:03:57 +0200

    Aloo Store

---
+++ android/app/src/main/kotlin/com/u6amtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aloo.storeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const robotoRegular = TextStyle(
+  fontFamily: 'NotoKufiArabic',
+  fontWeight: FontWeight.w400,
+);
+
+const robotoMedium = TextStyle(
+  fontFamily: 'NotoKufiArabic',
+  fontWeight: FontWeight.w500,
+);
+
+const robotoBold = TextStyle(
+  fontFamily: 'NotoKufiArabic',
+  fontWeight: FontWeight.w700,
+);
+
+const robotoBlack = TextStyle(
+  fontFamily: 'NotoKufiArabic',
+  fontWeight: FontWeight.w900,
+);
